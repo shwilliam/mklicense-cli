aafa77e2d6b2a6a762843b0339962a95ec4cb69c
William L <w-lindvall@outlook.com>
2019-10-11 11:18:32 -0700

    add support for gpl v3+

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,12 +1,15 @@
 import generateMIT from './mit'
 import generateISC from './isc'
+import generateGPL from './gpl'
 
-export default ({license, name, year}) => {
+export default ({license, desc, name, year}) => {
   switch (license) {
     case 'MIT':
       return generateMIT(name, year)
     case 'ISC':
       return generateISC()
+    case 'GPL':
+      return generateGPL(desc, name, year)
     default:
       break
   }
