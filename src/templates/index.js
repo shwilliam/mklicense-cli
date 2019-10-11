b35ea6edeb2b75a648a77c96bc5eb2ccb026f2cf
William L <w-lindvall@outlook.com>
2019-10-10 22:41:30 -0700

    feat: add support for isc

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,9 +1,12 @@
 import generateMIT from './mit'
+import generateISC from './isc'
 
 export default ({license, name, year}) => {
   switch (license) {
     case 'MIT':
       return generateMIT(name, year)
+    case 'ISC':
+      return generateISC()
     default:
       break
   }
