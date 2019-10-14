c867a6d0ecc1b4158a92a56529dc82ea49cc3296
Edin Kaymakqi <edinkaymakchi@gmail.com>
2019-10-14 11:05:32 -0700

    feat: add mpl support (#3)

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,6 +1,7 @@
 import generateMIT from './mit'
 import generateISC from './isc'
 import generateGPL from './gpl'
+import generateMPL from './mpl'
 
 export default ({license, desc, name, year}) => {
   switch (license) {
@@ -10,6 +11,8 @@ export default ({license, desc, name, year}) => {
       return generateISC()
     case 'GPL':
       return generateGPL(desc, name, year)
+    case 'MPL':
+      return generateMPL()
     default:
       break
   }
